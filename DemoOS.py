d9acfb996aa1a5805f8dc4edfc6089f02225b39e
zombie76 <zombi276@gmail.com>
2022-08-11 12:07:55 +0900

    파일목록 가져오기

---
+++ DemoOS.py
@@ -0,0 +1,19 @@
+#DemoOS.Po
+
+from os.path import *
+
+print(abspath("python.exe"))
+print(basename("c:\\work\\demo.py"))
+print(getsize("c:\\python39\\python.exe"))
+
+from os import *
+#system("notepad.exe")
+print("운영체제이름:", name)
+
+#파일 목록
+import glob
+
+result = glob.glob("c:\\work\\*.py")
+#print(result)
+for item in result:
+    print(item)
