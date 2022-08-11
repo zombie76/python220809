5ef51f9c2b0ac3d2caad5527c42fed4680da1850
zombie76 <zombi276@gmail.com>
2022-08-11 11:10:11 +0900

    정규표현식

---
+++ DemoStr.py
@@ -0,0 +1,29 @@
+# DemoStr.py
+
+strA = "python is very powerful"
+strB = "파이썬은 강력해"
+
+print( len(strA) )
+print( len(strB) )
+print( strA.capitalize() )
+print( strA.count("p") )
+print( strA.count("p", 7) )
+print( strA.startswith("py") )
+print( strA.endswith("py") )
+
+print( "MBC2580".isalnum() )
+print( "MBC:2580".isalnum() )
+print( "2580".isdecimal() )
+
+strC = "<<< spam and ham >>>"
+result = strC.strip("<> ")
+print(strC)
+print(result)
+
+print("---치환---")
+result = result.replace("spam", "spam egg")
+print(result)
+lst = result.split()
+print(lst)
+print("---다시 합치기---")
+print(":)".join(lst))
