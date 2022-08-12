7ff0cb00b090b846a8c5f0daaf7012b3845c61fa
zombie76 <zombi276@gmail.com>
2022-08-12 09:57:47 +0900

    네이버웹툰크롤링 완료

--- Web2.py
+++ Web2.py
@@ -1,25 +1,30 @@
 #Web2.py
 
 #웹서버에 요청
+from inspect import EndOfBlock
 import urllib.request
 from bs4 import BeautifulSoup
 
-data = urllib.request.urlopen(
-    "http://comic.naver.com/webtoon/list.nhn?titleId=20853&weekday=fri")
+#파일에 저장
+f = open("c:\\work\\webtoon.txt", "wt", encoding="utf-8")
 
-#검색이 용이한 객체
-soup = BeautifulSoup(data, "html.parser")
-cartoons = soup.find_all("td", class_="title")
-print("갯수:{0}".format(len(cartoons)))
-title = cartoons[0].find("a").text
-print(title)
-link = cartoons[0].find("a")["href"]
-print(link)
-#ctrl + /:다중라인 주석
-# <td class="title">
-# 	<a href="/webtoon/detail?titleId=20853&amp;no=50&amp;weekday=fri" onclick="nclk_v2(event,'lst.title','20853','50')">마음의 소리 50화 &lt;격렬한 나의 하루&gt;</a>
-# </td>
+#웹서버에 요청해서 실행결과 받기(문자열)
+for i in range(1,6):
+    #웹의 페이지별 주소 수정하여 입력
+    url = \
+        "https://comic.naver.com/webtoon/list?titleId=20853&weekday=fri&page=" + str(i)
 
-for item in cartoons:
-    title = item.find("a").text.strip()
-    print(title)
+    print(url)
+    data = urllib.request.urlopen(url)
+    #data = urllib.request.urlopen("http://comic.naver.com/webtoon/list.nhn?titleId=20853&weekday=fri")
+
+    #검색이 용이한 객체
+    soup = BeautifulSoup(data, "html.parser")
+    cartoons = soup.find_all("td", class_="title")
+ 
+    for item in cartoons:
+        title = item.find("a").text.strip()
+        print(title)
+        f.write(title + "\n")
+
+f.close()
