1f2d714946644a74c70e4c9d39939dc9e12d0c1a
zombie76 <zombi276@gmail.com>
2022-08-12 14:11:04 +0900

    Qt 수업 완료

--- DemoWebDriver.py
+++ DemoWebDriver.py
@@ -3,6 +3,7 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
 
 
 def set_chrome_driver():
@@ -15,8 +16,13 @@ driver = set_chrome_driver()
 driver.implicitly_wait(3)
 #driver.get('https://google.com')
 driver.get('https://nid.naver.com/nidlogin.login')
-driver.find_element('id').send_keys('kim')
-driver.find_element('pw').send_keys('1234')
+
+# driver.find_element('id').send_keys('kim')
+# driver.find_element('pw').send_keys('1234')
+userID = driver.find_elements(By.ID, 'id')[0]
+userPwd = driver.find_elements(By.ID, 'pw')[0]
+userID.send_keys("kim")
+userPwd.send_keys("1234")
 
 # driver.get('https://order.pay.naver.com/home')
 # html = driver.page_source
